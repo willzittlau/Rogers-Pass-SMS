5aee2ecfdb3e04253c436e4370eb00ad9a96220a
willzittlau <willzittlau@outlook.com>
2020-07-29 12:32:51 -0700

    Deploy to heroku

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import re
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 os.environ['TZ'] = 'UTC'
 time.tzset()
