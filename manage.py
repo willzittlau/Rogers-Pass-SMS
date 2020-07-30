1abc2622956b0adba7359e43c38b2394df069f1d
willzittlau <willzittlau@outlook.com>
2020-07-30 13:35:11 -0700

    Add sleep function to offset execution time with heroku's free scheduler

--- manage.py
+++ manage.py
@@ -1,15 +1,18 @@
 from flask_script import Manager
 from app import app
 from app import send_sms, webscrape
+import time
 
 manager = Manager(app)
 
 @manager.command
 def sms():
+    time.sleep(300)
     send_sms()
 
 @manager.command
 def scrape():
+    time.sleep(240)
     webscrape()
 
 if __name__ == "__main__":
