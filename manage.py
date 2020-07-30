1050878e31c08237e194abf869dd7c59ed2c774b
willzittlau <willzittlau@outlook.com>
2020-07-30 13:16:14 -0700

    Update schedule manager

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
 from flask_script import Manager
 from app import app
-from app import send_sms
+from app import send_sms, webscrape
 
 manager = Manager(app)
 
 @manager.command
 def sms():
     send_sms()
+
+@manager.command
+def scrape():
+    webscrape()
 
 if __name__ == "__main__":
     manager.run()
