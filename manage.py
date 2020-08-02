b5b42001282e9840dec723b49d3d2d20ff90f0d1
willzittlau <willzittlau@outlook.com>
2020-08-02 11:24:45 -0700

    update manager to perform both background tasks in one function

--- manage.py
+++ manage.py
@@ -15,5 +15,12 @@ def scrape():
     time.sleep(240)
     webscrape()
 
+@manager.command
+def both():
+    time.sleep(240)
+    webscrape()
+    time.sleep(60)
+    send_sms()
+
 if __name__ == "__main__":
     manager.run()
